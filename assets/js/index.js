f9f3e44ef379066d945c455486413a5ec6bdcdcf
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-21 21:30:32 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,26 +45,26 @@ function rndNum(min, max) {
 
 // ----------------------- 6 -----------------------
 
-// let array = [];
-// let ilgis = rndNum(20, 30);
+let array = [];
+let ilgis = rndNum(20, 30);
 
-// for (let i = 0; i < ilgis; i++) {
-//   array.push(rndNum(10, 30));
-// }
+for (let i = 0; i < ilgis; i++) {
+  array.push(rndNum(10, 30));
+}
 
-// let max = array[0];
-// let max2 = array[0];
+let max = array[0];
+let max2 = array[0];
 
-// for (let i = 0; i < array.length; i++) {
-//   if (max < array[i]) {
-//     max2 = max;
-//     max = array[i];
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  if (max < array[i]) {
+    max2 = max;
+    max = array[i];
+  }
+}
 
-// console.log("Pirma didžiausia reikšmė masyve: " + max);
-// console.log("Antra didžiausia reikšmė masyve: " + max2);
-// console.log(array);
+console.log("Pirma didžiausia reikšmė masyve: " + max);
+console.log("Antra didžiausia reikšmė masyve: " + max2);
+console.log(array);
 
 // ----------------------- 7 -----------------------
 
@@ -143,3 +143,61 @@ function rndNum(min, max) {
 //  }
 
 //  console.log(telefonoNumeris(3708856224));
+
+
+
+
+
+
+
+
+
+// PATAISYTI
+//------------------------------------------------- 8 -----------------------------------------------------
+// let a = [6, 4]
+// let b = [8, 2, 5]
+
+// const lygineSuma = (a, b) => {
+// let sum = 0
+//   if (typeof a == 'number' && typeof b == 'number')  {
+//     sum = a + b
+//   } if (sum % 2 != 0) {
+//   sum = 'Suma nelyginė'
+// } if (Array.isArray(a) == true && Array.isArray(b) == true){
+//   sum = a.length + b.length
+// }
+// return sum
+// }
+
+// console.log(lygineSuma(a, b));
+
+//------------------------------------------------- 9 -----------------------------------------------------
+// const pirminisNumeris = (a) => {
+//   let x = 0
+//   for (let i = 2, s = Math.sqrt(a); i <= s; i++){
+//     if (a % i === 0) {
+//       x = `${a} nera pirminis`
+//     } else  if (a % i !== 0){
+//       x = 'Pirminis: ' + a
+//     }
+//   }
+//   return x
+// };
+
+// console.log(pirminisNumeris(23));
+
+//------------------------------------------------- 10 -----------------------------------------------------
+
+// let a = [3, 7, 0, 8, 6, 8, 2, 9, 1, 4]
+
+// const telefonoNumeris = (a) => {
+//   let x = 0
+//   if (Array.isArray(a) != true || a.length != 10){
+//     x = 'Ne masyvas'
+//   } else {
+//   x = '('+a[0]+a[1]+a[2]+')'+' '+a[3]+a[4]+a[5]+'-'+a[6]+a[7]+a[8]+a[9]
+// }
+//   return x
+// }
+
+// console.log(telefonoNumeris(a));
